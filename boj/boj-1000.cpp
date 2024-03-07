99d64ac3d54758656de59ef5b26cfc9f54e4ff24
edward <gyurse@gmail.com>
2024-03-08 07:16:32 +0900

    initial commit

---
+++ boj/boj-1000.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <map>
+#include <stack>
+#include <deque>
+#include <set>
+#include <cmath>
+#include <algorithm>
+#include <cstring>
+
+#define pii pair<int,int>
+#define ll long long
+
+using namespace std;
+
+int main(int argc, char **argv){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+		
+	int A,B;
+	scanf("%d %d", &A,&B);
+	printf("%d",A+B);
+	return 0;
+}
