fe72e7f25adc5a40989ddf2fc15c269c89798ad5
edward <gyurse@gmail.com>
2024-06-23 14:59:55 +0900

    Update

--- boj/11653_소인수분해.cpp
+++ boj/11653_소인수분해.cpp
@@ -7,24 +7,11 @@ using namespace std;
 using pii = pair<int,int>;
 using ll = long long;
 
-constexpr int LM=10'000'000;
 int N;
-bool isprime[LM];
 
 int main(int argc, char **argv){
 	scanf("%d",&N);
 		
-	isprime[0] = false;
-	isprime[1] = false;
-
-	for(int i=2; i*i<LM; i++) {
-		if(!isprime[i]) continue;
-		isprime[i] = false;
-		for(int j=i*i; j<LM; j+=i) {
-			isprime[j] = false;
-		}
-	}
-	
 	int div=2;
 	while(N > 1) {
 		if(N % div == 0) {
@@ -34,14 +21,5 @@ int main(int argc, char **argv){
 		else{
 			div++;
 		}
-
-		if(div > N) {
-			if(isprime[N]) {
-				printf("%d", N);
-				break;
-			}
-		}
 	}
-
-
 }
