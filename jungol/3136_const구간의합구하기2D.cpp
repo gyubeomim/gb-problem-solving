f366bc3d799397b58c76b98342b5ad14f18eec18
edward <gyurse@gmail.com>
2025-02-08 00:31:10 +0900

    Update

--- jungol/3136_const구간의합구하기2D.cpp
+++ jungol/3136_const구간의합구하기2D.cpp
@@ -1,51 +1,25 @@
 #include <cstdio>
-int N, M;
-
 using ll = long long;
-constexpr int LM = (int)1004;
-ll **S;
+
+int N;
+int A[1005][1005];
+ll S[1005][1005];
 
 int main() {
 	scanf("%d", &N);
-
-	S = new ll*[LM];
-	for (int i = 0; i < LM; i++) {
-		S[i] = new ll[LM];
-	}
-
-	S[0][0] = 0;
-
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= N; j++) {
-			int num;
-			scanf("%d ", &num);
-
-			if (j - 1 < 0 && i - 1 < 0) {
-				S[i][j] =  num;
-			}
-			else if (j -1 < 0) {
-				S[i][j] = S[i-1][j] + num;
-			}
-			else if (i - 1 < 0) {
-				S[i][j] = S[i][j-1] + num;
-			}
-			else  {
-				S[i][j] = S[i-1][j] + S[i][j-1] - S[i-1][j-1] + num;
-			}
+			scanf("%d", &A[i][j]);
+			S[i][j] = S[i - 1][j] + S[i][j - 1] - S[i - 1][j - 1] + A[i][j];
 		}
 	}
 
+	int M, sr, sc, er, ec;
 	scanf("%d", &M);
-
-	ll ans = 0;
-	for (int i = 0; i < M; i++) {
-		int sr, sc, er, ec;
-		scanf("%d %d %d %d", &sr, &sc, &er, &ec);
-
-		ans = S[er][ec] - S[er][sc - 1] - S[sr - 1][ec] + S[sr - 1][sc - 1];
-		printf("%lld\n", ans);
+	while (M--) {
+		scanf("%d%d%d%d", &sr, &sc, &er, &ec);
+		ll sum = S[er][ec] - S[sr - 1][ec] - S[er][sc - 1] + S[sr - 1][sc - 1];
+		printf("%lld\n", sum);
 	}
-
-
-	return 0;
 }
+
