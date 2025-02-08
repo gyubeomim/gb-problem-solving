fc0bfb56a7dac8cb9dab72f7dd26e37ef1b925f0
edward <gyurse@gmail.com>
2025-02-08 09:58:39 +0900

    Update

--- algospot/소풍.cpp
+++ algospot/소풍.cpp
@@ -1,37 +1,43 @@
 #include <bits/stdc++.h>
-#define rnt register int
-#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
-#define WATCH(x) cout << #x << " : " << x << '\n';
-#define PRINT(x) cout << x << '\n';
-using namespace std;
-using pii = pair<int,int>;
-using ll = long long;
 
-int n;
+int N, M;
+bool taken[10];    // taken[i] = i번째 학생이 짝을 이미 찾았으면 true 아니면 false
 bool areFriends[10][10];
 
-// taken[i] = i번째 학생이 짝을 이미 찾았으면 true 아니면 false
 int countPairings(bool taken[10]){
-	// base condition : 모든 학생이 짝을 찾았으면 한 가지 방법을 찾았으니 종료한다
-	bool finished=true;
-	for(int i=0;i<n;i++) if(!taken[i]) finished = false;
-	if(finished) return 1;
+	int firstFree = -1;
+	for(int i=0; i<N; i++) if(!taken[i]) { firstFree = i; break; }
+	
+	if(firstFree == -1) return 1;
 
-	int ret=0;
-
-	// 서로 친구인 두 학생을 찾아 짝을 지어 준다
-	for(int i=0;i<n;i++){
-		for(int j=0;j<n;j++){
-			if(!taken[i] && !taken[j] && areFriends[i][j]) {
-				taken[i] = taken[j] = true;
-				ret += countPairings(taken);
-				taken[i] = taken[j] = false;
-			}
+	int ret = 0;
+	for(int pairWith = firstFree+1; pairWith < N; ++pairWith) {
+		if(!taken[pairWith] && areFriends[firstFree][pairWith]) {
+			taken[firstFree] = taken[pairWith] = true;
+			ret += countPairings(taken);
+			taken[firstFree] = taken[pairWith] = false;
 		}
 	}
 	return ret;
 }
 
 int main(int argc, char **argv){
-	 
+	int T;
+	scanf("%d", &T);
+	
+	while(T--){
+		scanf("%d %d", &N, &M);
+
+		memset(taken, 0, sizeof(taken));
+		memset(areFriends, 0, sizeof(areFriends));
+
+		for(int i=0; i<M; i++){
+			int a, b;
+			scanf("%d %d", &a, &b);
+			areFriends[a][b] = true;
+			areFriends[b][a] = true;
+		}
+		
+		printf("%d\n", countPairings(taken));
+	}
 }
