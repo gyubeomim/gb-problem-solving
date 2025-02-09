70b077af6533c67fe49b648e8e9504dcae43807d
Gyubeom Im <criterion.im@gmail.com>
2025-02-09 19:04:05 +0900

    Update

--- algospot/ch8_wildcard_와일드카드.cpp
+++ algospot/ch8_wildcard_와일드카드.cpp
@@ -7,6 +7,7 @@ using namespace std;
 using pii = pair<int,int>;
 using ll = long long;
 
+// solution #1
 bool match(const string& w, const string& s){
 	int pos = 0;
 	while(pos < s.size() 
@@ -26,24 +27,51 @@ bool match(const string& w, const string& s){
 	return false;
 }
 
+// solution #2 DP O(n^3)
+int dp[101][101];
+string W,S;
+
+bool match2(int w, int s){
+	int& ret = dp[w][s];
+	if(ret != -1) return ret;
+
+	while(s < S.size() && w < W.size() && 
+							(W[w] == '?' || W[w] == S[s])) {
+		++w;
+		++s;
+	}
+
+	if(w == W.size()) return ret = (s == S.size());
+
+	if(W[w] == '*') 
+		for(int skip = 0; skip+s <= S.size(); skip++)
+			if(match2(w+1, s+skip))
+				return ret = 1;
+		
+	return ret = 0;
+}
+
+
 int main(int argc, char **argv){
 		int C;
 		scanf("%d",&C);
 
 		while(C--){
 			string w;
-			cin >> w;
+			cin >> W;
 
 			int N;
 			scanf("%d", &N);
+
+			memset(dp, -1, sizeof(dp));
 			
 			vector<string> ans;
 			for(int i=0; i<N; i++){
 				string s;
-				cin >> s;
+				cin >> S;
 				
-				if(match(w,s)) 
-					ans.push_back(s);
+				// if(match(w,s))  ans.push_back(s);
+				if(match2(0,0))  ans.push_back(S);
 			}
 			
 			sort(ans.begin(), ans.end());
