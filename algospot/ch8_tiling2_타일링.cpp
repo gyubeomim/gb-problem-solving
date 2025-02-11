fffde1469fd4bee9e03c6549c2b86a776cdfa8a4
edward <gyurse@gmail.com>
2025-02-11 22:58:17 +0900

    Update

--- algospot/ch8_tiling2_타일링.cpp
+++ algospot/ch8_tiling2_타일링.cpp
@@ -1,12 +1,32 @@
 #include <bits/stdc++.h>
-#define rnt register int
-#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
-#define WATCH(x) cout << #x << " : " << x << '\n';
-#define PRINT(x) cout << x << '\n';
 using namespace std;
-using pii = pair<int,int>;
-using ll = long long;
+
+constexpr int MOD = 1000000007;
+
+int n;
+int dp[105];    
+// dp[n] : 2xn 크기의 직사각형을 채우는 방법의 수
+// dp[n] = (dp[n-1] + dp[n-2]) % MOD
+
+// 시간복잡도: O(n)  (부분문제 O(n), 계산 O(1))
+// 2*width 크기의 사각형을 채우는 방법의 수를 MOD로 나눈 나머지를 반환한다
+int tiling(int width) {
+	// 기저 사례: width가 1 이하일 때
+	if(width <= 1) return 1;
+
+	int& ret = dp[width];
+	if(ret != -1) return ret;
+	return ret = (tiling(width-2) + tiling(width-1)) % MOD;
+}
 
 int main(int argc, char **argv){
-	 
+	 int C; scanf("%d",&C);
+	 while(C--){
+		 scanf("%d", &n);
+
+		 memset(dp, -1, sizeof(dp));
+		 
+		 int ret = tiling(n);
+		 printf("%d\n", ret);
+	 }
 }
