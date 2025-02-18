8a3bb6b13f01215d366517f7c5f725fdf863f8b3
edward <gyurse@gmail.com>
2025-02-18 21:06:38 +0900

    Update

--- boj/1629_곱셈.cpp
+++ boj/1629_곱셈.cpp
@@ -6,10 +6,10 @@ ll A,B,C;
 
 ll POW(ll a, ll b, ll m){
 	if(b==1) return a%m;
-	ll val = POW(a, b/2, m);
-	val = val*val%m;
-	if(b%2==0) return val;
-	return val*a%m;
+	ll half = POW(a, b/2, m);
+	ll ret = (half*half)%m;
+	if(b%2==0) return ret;
+	else       return ret*a%m;
 }
 
 int main(int argc, char **argv){
