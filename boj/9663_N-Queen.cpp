6db480828e7b204f2dfd2069fbcee318bc11fa0e
Gyubeom Im <criterion.im@gmail.com>
2025-02-19 15:33:37 +0900

    Update

--- boj/9663_N-Queen.cpp
+++ boj/9663_N-Queen.cpp
@@ -8,29 +8,29 @@ using pii = pair<int,int>;
 using ll = long long;
 
 const int LM = 40;
-bool used1[LM];   // column |
-bool used2[LM];   // rtop diagonal /
-bool used3[LM];   // ltop diagonal \
+bool used1[LM];  // column |
+bool used2[LM];  // rtop diagonal /
+bool used3[LM];  // ltop diagonal \
 
 int cnt = 0;
 int N;
 
-void solve( int cur ) {
-  if ( cur == N ) {
+void solve( int r ) {
+  if ( r == N ) {
     cnt++;
     return;
   }
 
-  for ( int i = 0; i < N; i++ ) {
-    if ( used1[i] || used2[i + cur] || used3[cur - i + N - 1] ) continue;
+  for ( int c = 0; c < N; c++ ) {
+    if ( used1[c] || used2[r + c] || used3[r - c + N - 1] ) continue;
 
-    used1[i] = 1;
-    used2[i + cur] = 1;
-    used3[cur - i + N - 1] = 1;
-    solve( cur + 1 );
-    used1[i] = 0;
-    used2[i + cur] = 0;
-    used3[cur - i + N - 1] = 0;
+    used1[c] = 1;
+    used2[c + r] = 1;
+    used3[r - c + N - 1] = 1;
+    solve( r + 1 );
+    used1[c] = 0;
+    used2[c + r] = 0;
+    used3[r - c + N - 1] = 0;
   }
 }
 
