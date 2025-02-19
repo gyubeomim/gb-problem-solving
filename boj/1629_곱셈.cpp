3358327f909234cc7235198468f7ee6d7bfc7df4
Gyubeom Im <criterion.im@gmail.com>
2025-02-19 14:18:25 +0900

    Update

--- boj/1629_곱셈.cpp
+++ boj/1629_곱셈.cpp
@@ -2,16 +2,18 @@
 using ll = long long;
 using namespace std;
 
-ll POW(ll A, ll B, ll C){
-	if(B==1) return A%C;
-	ll half = POW(A, B/2, C);
-	ll ret = (half*half)%C;
-	if(B%2==0) return ret;
-	else       return ret*A%C;
+ll POW( ll A, ll B, ll C ) {
+  if ( B == 1 ) return A % C;
+  ll half = POW( A, B / 2, C );
+  ll ret = ( half * half ) % C;
+  if ( B % 2 == 0 )
+    return ret;
+  else
+    return ret * A % C;
 }
 
-int main(int argc, char **argv){
-	ll A,B,C;
-	scanf("%lld%lld%lld",&A,&B,&C);
-	printf("%lld", POW(A,B,C));
+int main( int argc, char **argv ) {
+  ll A, B, C;
+  scanf( "%lld%lld%lld", &A, &B, &C );
+  printf( "%lld", POW( A, B, C ) );
 }
