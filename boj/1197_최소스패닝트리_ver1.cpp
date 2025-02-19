91e387cce4fc2b82272559751b2777601f953909
edward <gyurse@gmail.com>
2025-02-19 18:57:08 +0900

    Update

--- boj/1197_최소스패닝트리_ver1.cpp
+++ boj/1197_최소스패닝트리_ver1.cpp
@@ -13,42 +13,42 @@ bool chk[LM]; // chk[i] : i번째 정점이 MST에 속해있는가?
 int cnt = 0;  // 현재 선택된 간선의 수
 
 int main() {
-	int v, e;
-	scanf("%d%d", &v, &e);
+  int v, e;
+  scanf("%d%d", &v, &e);
 
-	for (int i = 0; i < e; i++) {
-		int a, b, cost;
-		scanf("%d%d%d", &a, &b, &cost);
-		adj[a].push_back({ cost,b });
-		adj[b].push_back({ cost,a });
-	}
+  for (int i = 0; i < e; i++) {
+    int a, b, cost;
+    scanf("%d%d%d", &a, &b, &cost);
+    adj[a].push_back({ cost,b });
+    adj[b].push_back({ cost,a });
+  }
 
-	// Prim algorithm (using pq)
-	priority_queue<tiii, vector<tiii>, greater<tiii>> pq;   // pq[i] = { cost, vertex_id1, vertex_id2 }
+  // Prim algorithm (using pq)
+  priority_queue<tiii, vector<tiii>, greater<tiii>> pq;   // pq[i] = { cost, vertex_id1, vertex_id2 }
 
-	chk[1] = 1;
+  chk[1] = 1;
 
-	for (auto nxt : adj[1]) {
-		pq.push({ nxt.X, 1, nxt.Y });
-	}
+  for (auto nxt : adj[1]) {
+    pq.push({ nxt.X, 1, nxt.Y });
+  }
 
-	int ans = 0;
+  int ans = 0;
 
-	while (cnt < v - 1) {
-		int cost, a, b;
-		tie(cost, a, b) = pq.top(); pq.pop();
+  while (cnt < v - 1) {
+    int cost, a, b;
+    tie(cost, a, b) = pq.top(); pq.pop();
 
-		if (chk[b]) continue;
+    if (chk[b]) continue;
 
-		ans += cost;
-		chk[b] = 1;
-		cnt++;
+    ans += cost;
+    chk[b] = 1;
+    cnt++;
 
-		for (auto nxt : adj[b]) {
-			if (!chk[nxt.Y])
-				pq.push({ nxt.X,b,nxt.Y });
-		}
-	}
-	printf("%d\n", ans);
+    for (auto nxt : adj[b]) {
+      if (!chk[nxt.Y])
+        pq.push({ nxt.X,b,nxt.Y });
+    }
+  }
+  printf("%d\n", ans);
 }
 
--- boj/1197_최소스패닝트리_ver2.cpp
+++ boj/1197_최소스패닝트리_ver2.cpp
@@ -2,52 +2,52 @@
 using namespace std;
 using tiii = tuple<int,int,int>;
 
-vector<int> parent(10005,-1);
+vector<int> p(10005,-1);
 
 int find(int x){
-	if(parent[x] < 0) return x;
-	return parent[x] = find(parent[x]);
+  if(p[x] < 0) return x;
+  return p[x] = find(p[x]);
 }
 
-bool Union(int a, int b){
-	a = find(a); b = find(b);
-	if(a == b) return 0;
+bool isDiffGroup(int a, int b){
+  a = find(a); b = find(b);
+  if(a == b) return 0;
 
-	if(parent[a] == parent[b]) parent[a]--;
-	if(parent[a] < parent[b]) parent[b] = a;
-	else parent[a] = b;
+  if(p[a] == p[b]) p[a]--;
+  if(p[a] < p[b]) p[b] = a;
+  else p[a] = b;
 
-	return 1;
+  return 1;
 }
 
 int v, e;
 tiii edge[100005];
 
 int main(void) {
-	scanf("%d%d", &v, &e);
-	for(int i = 0; i < e; i++){
-		int a, b, cost;
-		scanf("%d%d%d",&a,&b,&cost);
-		edge[i] = {cost, a, b};
-	}
+  scanf("%d%d", &v, &e);
+  for(int i = 0; i < e; i++){
+    int a, b, cost;
+    scanf("%d%d%d",&a,&b,&cost);
+    edge[i] = {cost, a, b};
+  }
 
-	sort(edge, edge + e);
+  sort(edge, edge + e);
 
-	int cnt = 0;
-	int ans = 0;
+  int cnt = 0;
+  int ans = 0;
 
-	// Kruskal algorithm (using Union-find)
-	for(int i = 0; i < e; i++){
-		int a, b, cost;
-		tie(cost, a, b) = edge[i];
+  // Kruskal algorithm (using Union-find)
+  for(int i = 0; i < e; i++){
+    int a, b, cost;
+    tie(cost, a, b) = edge[i];
 
-		if(!Union(a, b)) continue;
+    if(!isDiffGroup(a, b)) continue;
 
-		ans += cost;
-		cnt++;
-		if(cnt == v-1) break;
-	}
+    ans += cost;
+    cnt++;
+    if(cnt == v-1) break;
+  }
 
-	printf("%d\n", ans);
+  printf("%d\n", ans);
 }
 
