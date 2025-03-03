7d8b8aa1e89019ecc5632b41bd98a474df8a21b8
edward <gyurse@gmail.com>
2025-03-03 16:14:49 +0900

    Update

--- algospot/ch8_poly_폴로오미노.cpp
+++ algospot/ch8_poly_폴로오미노.cpp
@@ -3,10 +3,10 @@
 const int MOD = 10000000;
 const int LM = 105;
 
-// dp[n][first] : n개의 정사각형을 포함하되, 첫 줄에 first개의 정사각형이 들어가는 폴리오미노 수
 int dp[LM][LM];
+// dp[n][first] : n개의 정사각형을 포함하되, 첫 줄에 first개의 정사각형이 들어가는 폴리오미노 수
+// poly(n, first) = sum_{second=1}^{n-first} (second+first-1) * poly(n-first, second)
 
-// n개의 정사각형이 주어졌을 때 첫 가로줄이 first개의 정사각형을 포함하는 폴리오미노의 수를 반환하는 함수
 // 시간복잡도: O(n^3) 
 // n, first의 조합의 수 O(n^2), poly 계산 O(n)
 int poly( int n, int first ) {
