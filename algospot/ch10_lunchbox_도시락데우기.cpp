5f217f36b80cf6d2a8d4599f1d4eba33a91377cc
edward <gyurse@gmail.com>
2025-03-03 21:13:07 +0900

    Update

--- algospot/ch10_lunchbox_도시락데우기.cpp
+++ algospot/ch10_lunchbox_도시락데우기.cpp
@@ -1,12 +1,7 @@
 #include <bits/stdc++.h>
-#define rnt register int
-#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
-#define WATCH(x) cout << #x << " : " << x << '\n';
-#define PRINT(x) cout << x << '\n';
 #define X first
 #define Y second
 using pii = std::pair<int,int>;
-using ll = long long;
 using namespace std;
 
 const int LM = 10005;
