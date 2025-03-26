f9e5580b4aa88cd221cf13a68d4eb0e8a37af0b1
edward <gyurse@gmail.com>
2025-03-26 21:49:07 +0900

    Update

--- algospot/ch18_josephus_조세푸스문제.cpp
+++ algospot/ch18_josephus_조세푸스문제.cpp
@@ -1,12 +1,4 @@
 #include <bits/stdc++.h>
-#define rnt register int
-#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
-#define WATCH(x) cout << #x << " : " << x << '\n';
-#define PRINT(x) cout << x << '\n';
-#define X first
-#define Y second
-using pii = std::pair<int,int>;
-using ll = long long;
 using namespace std;
 
 vector<int> men;
