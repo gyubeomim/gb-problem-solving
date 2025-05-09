ba58daf3eccf65131af94c72b95ced0a74635200
edward <gyurse@gmail.com>
2025-05-09 21:12:57 +0900

    Update

---
+++ jungol/4854_땅점령게임.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+
+int N,Q;
+int cnt[4];
+unordered_map<ll, int> hmap;   // [r*N+c, user]
+
+int main(int argc, char **argv){
+	scanf("%d%d",&N,&Q);
+
+	for (int i = 0; i < Q; i++) {
+		int user = i%4;
+		int r, c;
+		scanf("%d%d", &r, &c);
+
+		ll key = (ll)r*N + c;
+		auto it = hmap.find(key);
+		if(it == hmap.end()) {
+			hmap[key] = user;
+			cnt[user]++;
+		}
+		else {
+			if(it->second == user) {
+				cnt[user]--;
+				hmap.erase(it);
+			}
+			else {
+				if(cnt[user] < cnt[it->second]){
+					cnt[user]++;
+					cnt[it->second]--;
+					it->second = user;
+				}
+			}
+		}
+	}
+	for (int c : cnt) 
+		printf("%d\n", c);
+}
